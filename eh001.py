731453eee664bd96cf6d3fa6d4f319f2f9b00280
bala2051005E <150114895+bala2051005E@users.noreply.github.com>
2023-12-29 13:10:40 +0530

    Add files via upload

---
+++ eh001.py
@@ -0,0 +1,74 @@
+# try:
+#     print(5 / 0)
+#     # print(name)
+# except ZeroDivisionError:
+#     raise Exception("you can't divide by zero")
+# except NameError:
+#     print("you can't able to access variable without decalaration")
+# else:
+#     print("finnally excecuted without no error")
+# finally:
+#     print("finnally excecuted")
+
+# try:
+#     print(4/0)
+# except:
+#     print("something went wrong")
+# else:
+#     print("finally exceuted with no error")
+# finally:
+#     print("finally exceuted")            
+
+# try:
+#     print(5 / 0)
+#     # print(name)
+# except ZeroDivisionError:
+#     raise Exception("you can't divide by zero")
+# # except NameError:
+# #     print("you can't able to access variable without decalaration")
+# else:
+#     print("finnally excecuted without no error")
+# finally:
+#     print("finnally excecuted")
+
+# try:
+#     print(bala)
+# except NameError:
+#     print("you can't able to access vaiable without decalaaration")    
+# else:
+#     print("finally exceuted without no error")
+# finally:
+#     print("finally exceuted")    
+
+
+# try:
+#     print(bala)
+# except NameError:
+#     raise Exception("you can't able to access vaiable without decalaaration")    
+# else:
+#     print("finally exceuted without no error")
+# finally:
+#     print("finally exceuted")    
+
+
+# class A (Exception):
+#     pass
+# class B(A):
+#     pass  
+# class C(B):
+#     pass
+
+# for cls in [A,B,C]:
+#     try:
+#         raise cls()
+#     except C:
+#         print("C")
+#     except B:
+#         print("B")
+#     except A:
+#         print("A")        
+
+
+ 
+
+
---
+++ exceptional handing.py
@@ -0,0 +1,16 @@
+# try:
+#     print(2 / 2)
+
+# except:
+#     print("something went wrong")
+
+# finally:
+#     print("hence proved")    
+
+# x = 10
+# if x > 5:
+#     raise Exception('x should not exceed 5. The value of x was: {}'.format(x))
+
+# a = 12
+# if a >= 30:
+#     raise Exception("a should not exceed 30. The value of a was: {}".format(a))
